07f08e0b90b025bddebe9228df9617329bead584
Code Ass <aycabta@gmail.com>
2012-10-15 01:47:20 +0900

    removed chacking Class code

--- chained_arrow_notation.rb
+++ chained_arrow_notation.rb
@@ -17,17 +17,13 @@ def arrow(base, exponent, arrows)
 end
 
 def chained_arrow(chains)
-  if chains[-2].kind_of?(Integer) and chains[-2] == 1
+  if chains[-2] == 1
     chains.pop(2)
-  elsif chains[-1].kind_of?(Integer) and chains[-1] == 1
+  elsif chains[-1] == 1
     chains.pop
   end
 
-  is_all_of_integers = true
-  chains.each do |item|
-    is_all_of_integers = false if not item.kind_of?(Integer)
-  end
-  if chains.size <= 3 and is_all_of_integers
+  if chains.size <= 3
     arrow(chains[0], chains[1], chains[2].nil? ? 1 : chains[2])
   else
     y = chains.dup
